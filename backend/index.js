6172faabce72ac220aabf04cc042054a8a239977
1VishalSD <ilearnvk@gmail.com>
2024-06-12 21:32:42 +0530

    text

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const { createTodo } = require("./types");
+
 const app = express();
 
 app.use(express.json());
 
-app.post("/todo", function (req, res) {});
+app.post("/todo", function (req, res) {
+  const createPayload = req.body;
+});
 app.get("/todos", function (req, res) {});
 app.put("/completed", function (req, res) {});
