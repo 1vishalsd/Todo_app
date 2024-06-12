b978b94586d784c838e1508ecf5fdd16346c9c55
1VishalSD <ilearnvk@gmail.com>
2024-06-12 14:36:39 +0530

    zod validation

---
+++ backend/types.js
@@ -0,0 +1,14 @@
+const zod = require("zod");
+
+const createTodo = zod.object({
+  title: zod.string(),
+});
+
+const updateTodo = zod.object({
+  id: zod.string(),
+});
+
+module.exports = {
+  createTodo: createTodo,
+  updateTodo: updateTodo,
+};
