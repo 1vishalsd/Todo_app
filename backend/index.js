7d8f94507ce5c9f0b15d6a2b79ccad1361651312
1VishalSD <ilearnvk@gmail.com>
2024-06-13 21:34:57 +0530

    createTodo and UpdateTodo backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { createTodo } = require("./types");
+const { createTodo, updateTodo } = require("./types");
 
 const app = express();
 
@@ -8,6 +8,23 @@ app.use(express.json());
 
 app.post("/todo", function (req, res) {
   const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You set the wrong input",
+    });
+    return;
+  }
 });
 app.get("/todos", function (req, res) {});
-app.put("/completed", function (req, res) {});
+app.put("/completed", function (req, res) {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
+  if (!updatePayload.success) {
+    res.status(411).json({
+      msg: "You sent the wrong input",
+    });
+    return;
+  }
+});
