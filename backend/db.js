29d321cc53d6013f66785c7f38ab2114b219483b
1VishalSD <ilearnvk@gmail.com>
2024-06-20 06:38:30 +0530

    add backend db logic

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://Vishal:Vishal123@cluster0.efvbc6w.mongodb.net/todos"
+);
+const todoSchema = mongoose.Schema({
+  title: String,
+  descripation: String,
+  completed: booleand,
+});
+
+const todo = mongoose.model("todo", todoSchema);
+
+module.export = {
+  todo,
+};
