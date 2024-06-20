d4542b1fc55492f6b4fca421e305a2509101e6cd
1VishalSD <ilearnvk@gmail.com>
2024-06-20 06:08:38 +0530

    add validation of zod

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.post("/todo", function (req, res) {
-  const 
-});
+app.post("/todo", function (req, res) {});
 app.get("/todos", function (req, res) {});
 app.put("/completed", function (req, res) {});
--- todo_frontend/src/components/Todos.jsx
+++
@@ -1,9 +0,0 @@
-export function Todos() {
-  return (
-    <div>
-      <h1>go to the gym</h1>
-      <h2>you need to do gym </h2>
-      <button> mark is done</button>
-    </div>
-  );
-}
