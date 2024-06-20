a497f5cbe4adc881b14ead3356b2891d4d7c0d5f
1VishalSD <ilearnvk@gmail.com>
2024-06-20 11:02:46 +0530

    write code for how is the interface of todos and how to create todo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import CreateTodo from "./component/CreateTodo";
+import Todos from "./component/Todos";
 
 function App() {
   return (
     <div>
       <CreateTodo></CreateTodo>
+      <Todos></Todos>
     </div>
   );
 }
--- frontend/src/component/CreateTodo.jsx
+++ frontend/src/component/CreateTodo.jsx
@@ -4,18 +4,18 @@ function CreateTodo() {
   return (
     <div className="mt-20 w-1/2 m-auto justify-center flex flex-col shadow-lg p-4">
       <input
-        className=" p-3 m-1 outline rounded-sm  text-xl"
+        className=" p-3 m-1 outline rounded-sm  text-xl shadow-md"
         type="text"
         placeholder="title"
       />
       <br />
       <input
-        className="p-3 m-1 outline rounded-sm text-xl"
+        className="p-3 m-1 outline rounded-sm text-xl shadow-md"
         type="text"
         placeholder="description"
       />
       <br />
-      <button className="bg-green-600 p-3 w-1/2 m-auto rounded-3xl">
+      <button className="bg-green-600 p-3 w-1/2 m-auto rounded-3xl shadow-md">
         Add a Todo
       </button>
     </div>
